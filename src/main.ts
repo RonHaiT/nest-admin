953058317b354a7cb95a06bd5be71a59c63b6836
ronhai <122286911@qq.com>
2025-05-14 16:56:08 +0800

    '接口文档'

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,20 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/filter/http-exception/http-exception.filter';
 import { TransformInterceptor } from './common/interceptor/transform/transform.interceptor';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const options = new DocumentBuilder()
+    .setTitle('FS_ADMIN rbac系统') // 标题
+    .setDescription('后台管理系统接口文档') // 描述
+    .setVersion('1.0') // 版本
+    .build();
+  //配置swagger
+  const document = SwaggerModule.createDocument(app, options);
+  //配置swgger地址
+  SwaggerModule.setup('/docs/api', app, document);
+
   app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalInterceptors(new TransformInterceptor());
   await app.listen(process.env.PORT ?? 3000);
