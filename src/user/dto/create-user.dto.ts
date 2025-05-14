0fa03f056f1d3089900bbb2742671494278a0792
ronhai <122286911@qq.com>
2025-05-14 17:02:50 +0800

    '接口文档'

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,6 +1,23 @@
+import { ApiProperty } from '@nestjs/swagger';
 export class CreateUserDto {
+  @ApiProperty({
+    example: 'admin',
+    description: '用户名',
+  })
   username: string;
+  @ApiProperty({
+    example: '123456',
+    description: '密码',
+  })
   password: string;
+  @ApiProperty({
+    example: 'xxx@xx.com',
+    description: '用户邮箱',
+  })
   email: string;
+  @ApiProperty({
+    example: '18111111111',
+    description: '手机号',
+  })
   phone: string;
 }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -17,22 +17,34 @@ import { UpdateUserDto } from './dto/update-user.dto';
 import { ApiErrorCode } from 'src/common/enums/api-error-code.enum';
 import { ApiException } from 'src/common/filter/http-exception/api.exception.filter';
 import { CacheService } from 'src/cache/cache.service';
+import { ApiOperation, ApiTags, ApiOkResponse } from '@nestjs/swagger';
+import { CreateUserVo } from './vo/create-user.vo';
 
+@ApiTags('用户模块')
 @Controller('user')
 export class UserController {
   constructor(
     private readonly userService: UserService,
     private readonly cacheService: CacheService,
   ) {}
+  @ApiOperation({
+    summary: '设置redis', // 接口描述信息
+  })
   @Post('set')
   async setVal(@Body() val) {
     return await this.cacheService.set('name', 'rh');
   }
+  @ApiOperation({
+    summary: '获取redis', // 接口描述信息
+  })
   @Post('get')
   async getVal(@Body() key) {
     return await this.cacheService.get('name');
   }
-
+  @ApiOkResponse({
+    description: '返回示例',
+    type: CreateUserVo,
+  })
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto);
